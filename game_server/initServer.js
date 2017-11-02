2f881083ce90960ae83c6a487403f28a917c43f8
Smirnov-Mikhail <sm_m_a@mail.ru>
2017-11-02 14:48:41 +0300

    parse map

--- game_server/initServer.js
+++ game_server/initServer.js
@@ -1,13 +1,17 @@
-import _ from "lodash"
+const _ = require("lodash")
 
 module.exports = function() {
     var fs = require('fs');
     fs.readFile('./map.txt', "utf8", function(err, data) {
-        var splitData = data.split('\r\n');
+        var splitData = data.split('\n');
         var map = _.times(splitData[0].length, () => _.times(splitData.length, _.constant(null)))
         for (var i = 0; i < splitData.length; i++)
         {
-            //map.Add
+            var splitSplitData = splitData[i].split('')
+            for (var j = 0; j < splitData[i].length; j++)
+            {
+                map[i][j] = splitSplitData[i]
+            }
         }
     });
 }
